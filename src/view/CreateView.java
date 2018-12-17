5f7086725a5f3476bbca514139919ae26ebb510b
Jordan Phillips <jordan.m.phillips23@gmail.com>
2018-12-17 08:39:36 -0500

    initial

---
+++ src/view/CreateView.java
@@ -0,0 +1,84 @@
+package view;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+import javax.swing.JPanel;
+
+import controller.ViewManager;
+
+@SuppressWarnings("serial")
+public class CreateView extends JPanel implements ActionListener {
+	
+	private ViewManager manager;		// manages interactions between the views, model, and database
+	
+	/**
+	 * Constructs an instance (or object) of the CreateView class.
+	 * 
+	 * @param manager
+	 */
+	
+	public CreateView(ViewManager manager) {
+		super();
+		
+		this.manager = manager;
+		initialize();
+	}
+	
+	///////////////////// PRIVATE METHODS /////////////////////////////////////////////
+	
+	/*
+	 * Initializes the CreateView components.
+	 */
+	
+	private void initialize() {
+		
+		// TODO
+		//
+		// this is a placeholder for this view and should be removed once you start
+		// building the CreateView.
+		
+		this.add(new javax.swing.JLabel("CreateView", javax.swing.SwingConstants.CENTER));
+		
+		// TODO
+		//
+		// this is where you should build the CreateView (i.e., all the components that
+		// allow the user to enter his or her information and create a new account).
+		//
+		// feel free to use my layout in LoginView as an example for laying out and
+		// positioning your components.
+	}
+	
+	/*
+	 * CreateView is not designed to be serialized, and attempts to serialize will throw an IOException.
+	 * 
+	 * @param oos
+	 * @throws IOException
+	 */
+	
+	private void writeObject(ObjectOutputStream oos) throws IOException {
+		throw new IOException("ERROR: The CreateView class is not serializable.");
+	}
+	
+	///////////////////// OVERRIDDEN METHODS //////////////////////////////////////////
+	
+	/*
+	 * Responds to button clicks and other actions performed in the CreateView.
+	 * 
+	 * @param e
+	 */
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		// TODO
+		//
+		// this is where you'll setup your action listener, which is responsible for
+		// responding to actions the user might take in this view (an action can be a
+		// user clicking a button, typing in a textfield, etc.).
+		//
+		// feel free to use my action listener in LoginView.java as an example.
+	}
+}
